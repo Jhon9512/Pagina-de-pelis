31177239487c6bf644a99034cfa4e0b943de1729
Jhon9512 <71728649+Jhon9512@users.noreply.github.com>
2024-02-28 16:59:45 -0500

    Add files via upload

---
+++ ProyectoPE/PaginadePeliculas-main/main.js
@@ -0,0 +1,33 @@
+/*Menu*/
+
+function myFunction() {
+  var x = document.getElementById("menuvar");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
+
+/*Carousel*/
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("galeria");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
+}
+
